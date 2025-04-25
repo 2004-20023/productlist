b4a64721f94c3b06e6e6a496c1cf797ceee9bb84
Gowtham M <135612455+GowthamMurukesh@users.noreply.github.com>
2025-04-25 20:25:13 +0530

    table correction

--- src/app/maincontent/maincontent.component.ts
+++ src/app/maincontent/maincontent.component.ts
@@ -8,19 +8,47 @@ import { NgFor, NgIf } from '@angular/common';
   styleUrl: './maincontent.component.scss'
 })
 export class MaincontentComponent implements OnInit {
+product: any;
+getFilteredProductCount() {
+throw new Error('Method not implemented.');
+}
   ProductList: any = ProductList
   uniqueCategories: any = []
   selectedCategory: string = '';
+  currentPage: number = 1;
+  itemsPerPage: number = 10;
+  paginatedProducts: any[] = [];
+
   ngOnInit(): void {
     this.ProductList = this.ProductList.default
     this.uniqueCategories = [...new Set(this.ProductList.map((product: any) => product.category))];
+    this.updatePaginatedProducts();
+
     console.log("Product List:", this.ProductList)
     console.log("uniqueCategories:", this.uniqueCategories)
   }
 
   onCategoryChange(event: any) {
+    const selectedValue = event.value || '';
     console.log("Selected Category:", event.value)
-    this.selectedCategory = event.value;
+    this.selectedCategory = selectedValue;
+    this.currentPage = 1;
+    this.updatePaginatedProducts();
+
   }
 
+  updatePaginatedProducts() {
+    const filteredProducts = this.ProductList.filter(
+      (product: any) =>
+        product.category == this.selectedCategory || this.selectedCategory == ''
+    );
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedProducts = filteredProducts.slice(startIndex, endIndex);
+  }
+
+  changePage(page: number) {
+    this.currentPage = page;
+    this.updatePaginatedProducts();
+  }
 }
